d6af3fb847afaee30d8f39dba785f1bc2ccd6170
Pat Cichocki <pat.cichocki.uk@gmail.com>
2020-01-15 15:43:21 +0000

    initial structure implementation

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,3 +1,79 @@
 import React from 'react';
+import '../App.css';
+
+const Cards = props => {
+    return( 
+        <div>
+            <section className="upperSection">
+                <span className="timer"></span>
+                <span className="moves"></span>
+            </section>
+            <section className="cardGrid">
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardOne"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardTwo"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardThree"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardFour"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardFive"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer clickedCard">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardSix"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardSeven"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardEight"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront " id="cardNine"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardTen"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardEleven"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+
+                <div className="cardContainer">
+                    <div className="cardBack" >This is back</div>
+                    <div className="cardFront" id="cardTwelve"><img className="image" src="http://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg" alt="card image"/></div>
+                </div>
+    
+
+            </section>
+        </div>
+    );
+}
 
 export default Cards;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,12 @@
 import React from 'react';
+import '../App.css';
+
+const Footer = props => {
+    return (
+        <div>
+            <p className="feedback">Nice one!</p>
+        </div>
+    )
+}
 
 export default Footer;
