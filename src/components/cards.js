66c9989a55359d4033cce5987f04a10895335999
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-01-16 17:15:26 +0000

    completed MVPgit add .!

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,16 +2,30 @@ import React from 'react';
 import '../App.css';
 import { getData } from './header.js';
 
+const clickedCards = [];
+
 const Cards = props => {
   console.log('i am the array of photos in cards', props.photoArray);
-  
-  const [flipped, setFlip] = React.useState(false);
 
-  const flipper = (event) => {
+  const [flipped, setFlip] = React.useState(false);
+  const flipper = event => {
     setFlip(true);
     event.target.parentElement.classList.toggle('clickedCard');
-  }
+    clickedCards.push(event.target.parentElement);
+    console.log('I am the clicked cards', clickedCards);
 
+    if (clickedCards.length > 2) {
+      console.log('i am the first element', clickedCards[0]);
+      if (clickedCards[1].dataset.id === clickedCards[2].dataset.id) {
+        clickedCards[1].classList.toggle('clickedCard');
+        clickedCards[2].classList.toggle('clickedCard');
+        clickedCards[1].classList.add('correctCard');
+        clickedCards[2].classList.add('correctCard');
+      }
+      clickedCards[0].classList.toggle('clickedCard');
+      clickedCards.shift();
+    }
+  };
 
   return (
     <div>
@@ -20,18 +34,24 @@ const Cards = props => {
         <span className='moves'></span>
       </section>
       <section className='cardGrid'>
-          {props.photoArray.map(photo => (
-            <div onClick={flipper} className="cardContainer"  key={photo[0]}>
-              <div className='cardBack'>This is back</div>
-              <div className='cardFront' >
-                <img className='image' key={photo[0]} src={photo[1]} alt='card image' />
-              </div>
+        {props.photoArray.map(photo => (
+          <div
+            onClick={flipper}
+            className='cardContainer'
+            data-id={photo[0]}
+            key={photo[0]}
+          >
+            <div className='cardBack'>This is back</div>
+            <div className='cardFront'>
+              <img
+                className='image'
+                key={photo[0]}
+                src={photo[1]}
+                alt='card image'
+              />
             </div>
-
-            
-            
-          ))}
-        
+          </div>
+        ))}
       </section>
     </div>
   );
---
+++ src/gameLogic.js
@@ -0,0 +1,16 @@
+// function gameLogic {
+// how many cards have the "clicked" class?
+// if >1 {
+//   Add "currentDisplayedCard" class to current card
+//   Does the url f currentdisplayedcard match the url of previousdisplayedcard?
+//   if no {
+//     remove the class
+//   }
+// } if yes {
+//   remove the class
+//   add a class which displays the front
+// }
+// if 1 {
+// add the previousdisplayedcard class to the clicked card
+// }
+// }
